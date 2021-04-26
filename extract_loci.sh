d9059bed17082661dfee3c50c44decbcfc19e61e
DennisLarsson <43995478+DennisLarsson@users.noreply.github.com>
2021-04-26 10:42:20 +0200

    Update extract_loci.sh

--- extract_loci.sh
+++ extract_loci.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 #version 1.3
 
+echo "assembly	nr_locus" > locus_count2
+
 Names=`grep "Kept" populations*/populations.log | cut -f 1 -d"/"` #Extracts the names of the assemblies
 NamesArray=(${Names})
-Loci=`grep "Kept" populations*/populations.log | cut -f 2 -d" "` #extracts the locus count for each assembly
-LociArray=(${Loci})
 
-echo "assembly	locus_count" > locus_count
-
-i=0
-while [ $i -lt ${#NamesArray[@]} ]
-	do
-	echo "${NamesArray[$i]}	${LociArray[$i]}" >> locus_count #Prints name and locus count to file
-	i=$(( $i + 1 ))
+for i in ${NamesArray[@]}
+do
+	Loci=`grep -v "#" ./$i/populations.sumstats.tsv | cut -f 1 |  uniq | wc -l` #extracts the locus count for each assembly
+	echo "${i}	$Loci" >> locus_count2
 done
